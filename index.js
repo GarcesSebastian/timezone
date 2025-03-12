497e4b203ba3521f0ca3a995182487549926e932
soyvillareal <sebastiangarces152@gmail.com>
2025-03-12 17:06:05 -0500

    feature: i modify json return

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import hpp from "hpp";
 import xss from "xss-clean";
 import rateLimit from "express-rate-limit";
 import dotenv from "dotenv";
+import fetch from "node-fetch";
 
 dotenv.config();
 
@@ -12,9 +13,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(helmet());
-
 app.use(xss());
-
 app.use(hpp());
 
 const limiter = rateLimit({
@@ -24,9 +23,24 @@ const limiter = rateLimit({
 });
 app.use(limiter);
 
-app.get("/", (req, res) => {
-  const date = new Date().toISOString();
-  res.json({ time: date });
+app.get("/", async (req, res) => {
+  try {
+    const date = new Date().toISOString();
+    const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress; 
+    const response = await fetch(`http://ip-api.com/json/${ip}`);
+    const data = await response.json();
+
+    res.json({
+      time: date,
+      country: data.country || "Desconocido",
+      countryCode: data.countryCode || "N/A",
+      city: data.city || "Desconocida",
+      ip: ip || "No disponible"
+    });
+  } catch (error) {
+    console.error("Error obteniendo el país:", error);
+    res.status(500).json({ message: "Error obteniendo la información del país." });
+  }
 });
 
 app.use((err, req, res, next) => {
